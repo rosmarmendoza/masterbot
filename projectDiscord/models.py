5825ee586119435c6d8948b35b38f6ae09ab637a
Rosmar Mendoza <rosmarmendoza@MacBook-Air-de-Rosmar.local>
2022-09-29 18:05:40 -0500

    DataBase model

--- projectDiscord/models.py
+++ projectDiscord/models.py
@@ -1,5 +1,23 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
+
+""" Modelando la Base de Datos de Master Bot """
 
 class UserLogIn(models.Model):
     name = models.CharField(max_length=30)
+    password = models.CharField(max_length=20)
+    remember_password = models.CharField(max_length=20)
+
+class UserSignUp(models.Model):
+    name = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=30)
+    email = models.EmailField(max_length=30)
+    password = models.CharField(max_length=30)
+
+class Bot(models.Model):
+    name = models.CharField(max_length=30)
+    token = models.CharField(max_length=30)
+    welcome_message = models.TextField(300)
     
+
+
